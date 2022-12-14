453b81363d5feef1a5234d5ca87af25522573198
sayedhany <sayed591999@gmail.com>
2022-12-15 00:48:34 +0200

    Features and CTA sections

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -15,7 +15,7 @@ const Menu = () => {
         <a href="#possibility">Open AI</a>
       </p>
       <p>
-        <a href="features">Case Studies</a>
+        <a href="#features">Case Studies</a>
       </p>
       <p>
         <a href="#blog">Library</a>
